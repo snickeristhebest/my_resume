bdf6bf59fa96c8ca21846ccceac03877b4046587
snickeristhebest <nicoh7242@gmail.com>
2022-11-22 09:03:18 -0600

    Initial commit

--- resume.py
+++ resume.py
@@ -1,1 +1,19 @@
-#my name is nick
+# import libraries
+from flask import Flask, render_template
+
+#create our app
+
+app = Flask(__name__)
+
+#Create our directory
+
+@app.route('/')
+def resume():   
+    return render_template("index.html")
+  
+
+#start app
+if __name__ == "__main__":
+    app.run(
+        debug=True
+    )
